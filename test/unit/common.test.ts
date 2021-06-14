a3115db80c54973ceaf0b38cb00a6c3a4f986885
Matthieu Napoli <matthieu@mnapoli.fr>
2021-06-14 20:15:20 +0200

    Add tests to cover variable resolution

--- test/unit/common.test.ts
+++ test/unit/common.test.ts
@@ -5,10 +5,41 @@ describe("common", () => {
         const { cfTemplate } = await runServerless({
             fixture: "common",
             configExt: pluginConfigExt,
-            cliArgs: ["package"],
+            command: "package",
         });
         expect(cfTemplate.Resources).toMatchObject({
             UserDefinedResource: {},
+        });
+    });
+
+    it("should resolve variables", async () => {
+        const { cfTemplate } = await runServerless({
+            fixture: "variables",
+            configExt: pluginConfigExt,
+            command: "package",
+        });
+        expect(cfTemplate.Resources.FooLambdaFunction).toMatchObject({
+            Properties: {
+                Environment: {
+                    Variables: {
+                        VAR1: "",
+                    },
+                },
+            },
+        });
+        expect(cfTemplate.Resources.BarWorkerLambdaFunction).toMatchObject({
+            Properties: {
+                Environment: {
+                    Variables: {
+                        VAR2: "dev",
+                    },
+                },
+            },
+        });
+        expect(cfTemplate.Resources.UserDefinedResource).toMatchObject({
+            Properties: {
+                BucketName: "",
+            },
         });
     });
 });
